1c9ff6ca1a8558be457e01e62787c8d2eddda4a1
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2023-05-27 11:34:50 -0300

    Basic Mall Built

---
+++ Assets/Scripts/AIGrandma.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIGrandma : MonoBehaviour
+{
+    private float TimeStartedState;             // timer to know when we started a state
+    public LayerMask wallLayer;
+
+    [Header("Agent")]
+    public NavMeshAgent grandma;
+    public GameObject[] shops;
+    [SerializeField] float stoppedTime = 3f;    // how long the agent will remain in a shop
+    [SerializeField] float shopTime = 12f;      // how long the agent will shop
+    private bool isencouraged = false;          // is grandma encouraged
+    private Vector3 destination;                // placeholder for any destination the grandma needs
+    private float timer;                        // placeholder for timer
+    
+    public enum States
+    {
+        stopped,       // stopped = 0
+        shopping,     // shopping = 1
+        chasing,       // chasing = 2
+        goinghome,      // goinghome = 3
+        encouraged      // encouraged = 4
+    }
+    private States _currentState = States.stopped;       //sets the starting enemy state
+    public States currentState 
+    {
+        get => _currentState;
+        set {
+            if (_currentState != value) 
+            {
+                // Calling ended state for the previous state registered.
+                OnEndedState(_currentState);
+                
+                // Setting the new current state
+                _currentState = value;
+                
+                // Registering here the time we're starting the state
+                TimeStartedState = Time.time;
+                
+                // Call the started state method for the new state.
+                OnStartedState(_currentState);
+            }
+        }
+    }
+    // OnStartedState is for things that should happen when a state first begins
+    public void OnStartedState(States state)
+    {
+        switch (state) 
+        {
+            case States.stopped:
+                Debug.Log("I am " + currentState);
+                break;
+            case States.shopping:
+                Debug.Log("I am " + currentState);
+                destination = ShopDestination();                    // gets a destination to go shopping
+                break;
+            case States.chasing:
+                Debug.Log("I am " + currentState);
+                break;
+            case States.goinghome:
+                Debug.Log("I am " + currentState);
+                break;
+            case States.encouraged:
+                Debug.Log("I am " + currentState);
+                break;
+        }
+    }
+    // OnUpdatedState is for things that occur during the state (main actions)
+    public void OnUpdatedState(States state) 
+    {
+        switch (state) 
+        {
+            case States.stopped:
+                if (TimeElapsedSince(TimeStartedState, stoppedTime))
+                {
+                    currentState = States.shopping;
+                }
+                break;
+            case States.shopping:
+                if (TimeElapsedSince(TimeStartedState, shopTime))
+                {
+                    currentState = States.stopped;
+                }
+                break;
+            case States.chasing:
+                break; 
+            case States.goinghome:
+                break;
+            case States.encouraged:
+                break;
+        }
+    }
+    // OnEndedState is for things that should end or change when a state ends; for cleanup
+    public void OnEndedState(States state)
+    {
+        switch (state) 
+        {
+            case States.stopped:
+                break;
+            case States.shopping:
+                break;
+            case States.chasing:
+                break;
+            case States.goinghome:
+                break;
+            case States.encouraged:
+                break;
+        }
+    }
+    
+    void Start()
+    {
+        OnStartedState(currentState);
+    }
+
+    void Update()
+    {
+        OnUpdatedState(currentState);
+    }
+
+    private Vector3 ShopDestination()
+    {
+        return transform.position;
+    }
+
+    // This method can be used to test if a certain time has elapsed since we registered an event time. 
+    public bool TimeElapsedSince(float timeEventHappened, float testingTimeElapsed) => !(timeEventHappened + testingTimeElapsed > Time.time);
+    
+    // I use Handles.Label to show a label with the current state above the player. Can use it for more debug info as well.
+    // I wrap it around a #if UNITY_EDITOR to make sure it doesn't make its way into the build, unity doesn't like using UnityEditor methods in builds.
+    #if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        //If we're not playing don't draw gizmos.
+        if (!Application.isPlaying) return;
+        
+        //Setting the position for our debug label and the color.
+        Vector3 debugPos = transform.position;
+        debugPos.y += 2; 
+        GUI.color = Color.black;
+        UnityEditor.Handles.Label(debugPos,$"{currentState}");
+    }
+    #endif
+}
